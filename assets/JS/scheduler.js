4239bcdff0df2a2565569c577182da7b7e0bb678
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-28 16:30:12 +0000

    hidding test points

--- assets/JS/scheduler.js
+++ assets/JS/scheduler.js
@@ -1,12 +1,13 @@
 // Display the current day at the top of the calender when a user opens the planner.
-console.log("calendar")   // start test point
+// start test point: console.log("calendar")   
 
 // Current date and time --------------------------------------------------------------
 var todayDate = moment().format('LLLL');                  // note: default - display in local time - for certain zones use moment()UTC.format('LLLL'); or use other timezone (GMT)
 $("#currentDay").html(todayDate);
 console.log(todayDate) // test point
 
-//-------------------------------------------------------------------
+
+//---------------------------- time dependant txt area color change ---------------------------------------
 
 
 var currentTime = moment().hour()
@@ -16,7 +17,7 @@ var currentTime = moment().hour()
 
 $("textarea").each(function () {    //  textarea .event-descryption  .time-block   (may need change)
 var scheduleTime = $(this).attr("id")
-            console.log(scheduleTime) // test point - schedule by hrs
+            // console.log(scheduleTime) // test point - schedule by hrs
         
             if (scheduleTime == currentTime){
                 $(this).addClass("present");
@@ -31,6 +32,8 @@ var scheduleTime = $(this).attr("id")
             }       
         
 });
+
+// 
 
 
 
