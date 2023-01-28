d9a2a5f2faf77090617ec01c2d6ec91c825f7c79
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-28 11:01:08 +0000

    removing unnecesery comments

--- assets/JS/scheduler.js
+++ assets/JS/scheduler.js
@@ -1,29 +1,14 @@
 // Display the current day at the top of the calender when a user opens the planner.
+console.log("calendar")   // start test point
 
-console.log("calendar")   // test point
-
-
+// Current date and time --------------------------------------------------------------
 var todayDate = moment().format('LLLL');                  // note: default - display in local time - for certain zones use moment()UTC.format('LLLL'); or use other timezone (GMT)
 $("#currentDay").html(todayDate);
 console.log(todayDate) // test point
 
+//-------------------------------------------------------------------
 
 
 
 
 
-
-/*
-const store = window.localStorage;
-
-const now = moment.utc();
-
-
- const currentTime = {
-    text: moment.utc().format(LLLL),
-
-}; 
-
-$("#currentDay").text(now.format(LLLL));
-
-*/ 
