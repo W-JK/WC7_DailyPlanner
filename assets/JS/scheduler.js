dfc3c6c0f2355f69c3e3d0e655e7a5e6804e7749
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-29 17:27:45 +0000

    progres save - Sat tests

--- assets/JS/scheduler.js
+++ assets/JS/scheduler.js
@@ -1,5 +1,6 @@
 // Display the current day at the top of the calender when a user opens the planner.
 // start test point: console.log("calendar")   
+const localStore = window.localStorage;
 
 // Current date and time --------------------------------------------------------------
 var todayDate = moment().format('LLLL');                  // note: default - display in local time - for certain zones use moment()UTC.format('LLLL'); or use other timezone (GMT)
@@ -35,21 +36,61 @@ var scheduleTime = $(this).attr("id")
 
 // ------------------ save button function ------------------------ //
 
+saveEvent = e => {
+    console.log("clicked save button")
+    e.preventDefault();
+    
+    console.log("event function trigger");
+
+  var eventTime = document.querySelector('textarea').id
+    console.log(eventTime); // test point timeValue read
+  var textValue = document.querySelector('.descryption').value;
+    console.log(textValue); // test point textValue read
 
 
-/* 
-$(".saveBtn").click(function(saveEvent){
-    saveEvent.preventDefault();
-    var textValue =$(this)
-    var timeValue = $(this).
-
-    localStorage.setItem(timeValue,textValue);
-
-    console.log(textValue)
-    console.log(timeValue)
+    // test point:
+    // localStore.setItem(eventTime,textValue);
+     $("#8 .desryption").val(localStore.setItem(eventTime,textValue))
+     $("#9 .desryption").val(localStore.setItem(eventTime,textValue))
+    // localStore.setItem(timeValue,textValue);
 
 
-}); */
+};
+
+
+
+
+
+
+/* alternative solution
+var saveEventButton = document.querySelector("#saveEventBtn");
+console.log("saveEvent");
+
+
+
+saveEventButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+  var timeValue = document.querySelector('.hour').value;
+    console.log(timeValue); // test point timeValue read
+  var textValue = document.querySelector('.descryption').value;
+    console.log(textValue); // test point textValue read
+
+    ///  displayMessage(".future", "Registered successfully");
+
+    localStore.setItem(timeValue,textValue);
+
+
+}); 
+
+// ---------------------------------------------
+
+    const scheduleEvent = {
+        eventTime : hour.value,
+        eventText : descryption.value
+    }; */ // oryginal
+
+
 
 
 
